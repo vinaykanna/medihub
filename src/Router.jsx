989b39d7d57c42f2a5c60466c16b58d6f8a20ddf
vinaydazn <vinaykanna367@gmail.com>
2024-12-13 22:34:37 +0530

    create doctor consultation module

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,11 +5,16 @@ import Login from "@pages/Login/Login";
 import ContactUs from "@pages/ContactUs/ContactUs";
 import TermsAndConditions from "@pages/TermsAndConditions/TermsAndConditions";
 import PrivacyAndPolicy from "@pages/PrivacyAndPolicy/PrivacyAndPolicy";
+import FindADoctor from "@modules/doctor-consultation/FindADoctor/FindaADoctor";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route
+          path="/doctor-consultation/find-a-doctor"
+          element={<FindADoctor />}
+        />
         <Route path="/" element={<Welcome />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact-us" element={<ContactUs />} />
---
+++ src/modules/doctor-consultation/FindADoctor/FindaADoctor.jsx
@@ -0,0 +1,5 @@
+function FindADoctor() {
+  return <h1 className="mt-10 text-center text-4xl">Find a Doctor</h1>;
+}
+
+export default FindADoctor;
