32b38b93f7f2650f4ce4395621bf27e1df54e81d
vinaydazn <vinaykanna367@gmail.com>
2024-12-14 00:28:10 +0530

    build different login components for different login types

---
+++ src/components/SelectField.jsx
@@ -0,0 +1,46 @@
+import { twJoin, twMerge } from "tailwind-merge";
+import Chevron from "@assets/icons/Chevron";
+
+function SelectField({
+  children,
+  className,
+  selectClassName,
+  label,
+  defaultOptionLabel = "Select",
+  ...rest
+}) {
+  return (
+    <div className={twMerge("relative", className)}>
+      {label && (
+        <label
+          htmlFor={rest.id || rest.name}
+          className="text-primary-solid text-base mb-2 block ml-2"
+        >
+          {label}
+        </label>
+      )}
+      <div className="relative">
+        <select
+          className={twMerge(
+            "px-4 py-2 border-[1px] border-primary-solid border-solid",
+            "rounded-full bg-white outline-none text-[#BDBDBD]",
+            "placeholder:text-secondary-solid cursor-pointer appearance-none",
+            selectClassName
+          )}
+        >
+          <option value="">{defaultOptionLabel}</option>
+          {children}
+        </select>
+        <Chevron
+          fill="#E77E3A"
+          className={twJoin(
+            "rotate-90 absolute w-3 h-3 right-5",
+            "top-1/2 -translate-y-1/2 pointer-events-none"
+          )}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SelectField;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,11 +4,27 @@ import Header from "@components/Header";
 import { Link } from "react-router-dom";
 import Button from "../../components/Button";
 import { twJoin } from "tailwind-merge";
+import { useState } from "react";
+import { loginTitles } from "@constants/login.constants";
+import SelectField from "../../components/SelectField";
 
 function Login() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const [loginType, setLoginType] = useState("corporate");
+  const [otpSent, setOtpSent] = useState(false);
+  // const [otp, setOtp] = useState("");
+  // const [state, setState] = useState({
+  //   corporateEmail: "",
+  //   password: "",
+  //   policy: "",
+  //   policyNumber: "",
+  //   phoneNumber: "",
+  // });
+
+  const handleOtp = () => {
+    setOtpSent(true);
   };
+
+  const handleSubmit = () => {};
   return (
     <>
       <section
@@ -33,75 +49,164 @@ function Login() {
               "rounded-3xl shadow-[0_4px_20px_0px_#E77E3A33]"
             )}
           >
-            <form onSubmit={handleSubmit}>
-              <header>
-                <h1 className="text-3xl leading-[40px] font-nunito-bold text-primary-solid">
-                  Corporate Login
-                </h1>
-                <p className="text-secondary-solid font-nunito-bold text-base mt-2 font-bold">
-                  Your Digital Health Hub
-                </p>
-              </header>
-              <main className="mt-8">
-                <TextField
-                  id="corporate_email"
-                  placeholder="Enter your corporate email ID"
-                  label="Enter Corporate Email ID"
-                  className="text-left"
-                  autoFocus
-                  required
-                  type="email"
-                  inputClassName="placeholder:text-[#BDBDBD]"
-                />
-                <TextField
-                  id="password"
-                  placeholder="Enter Password"
-                  label="Enter Password"
-                  className="text-left mt-5"
-                  required
-                  type="password"
-                  inputClassName="placeholder:text-[#BDBDBD]"
-                />
-                <Button type="submit" className="w-full mt-7">
+            <header>
+              <h1 className="text-3xl leading-[40px] font-nunito-bold text-primary-solid">
+                {loginTitles[loginType]}
+              </h1>
+              <p className="text-secondary-solid font-nunito-bold text-base mt-2 font-bold">
+                Your Digital Health Hub
+              </p>
+            </header>
+            <main className="mt-8">
+              <Fields loginType={loginType} otpSent={otpSent} />
+              {loginType === "phone" && !otpSent ? (
+                <Button onClick={handleOtp} className="w-full mt-7">
+                  Get OTP
+                </Button>
+              ) : (
+                <Button onClick={handleSubmit} className="w-full mt-7">
                   Login
                 </Button>
-              </main>
-            </form>
+              )}
+            </main>
             <footer className="mt-3">
-              <p className="text-sm leading-6">
-                By signing in you agree to our <br />{" "}
-                <Link
-                  to="/terms-and-conditions"
-                  className="text-secondary-solid"
-                >
-                  Terms and Conditions
-                </Link>{" "}
-                and{" "}
-                <Link to="/privacy-policy" className="text-secondary-solid">
-                  Privacy Policy
-                </Link>
-              </p>
+              <TermsAndPrivacy />
               <div className="flex gap-2 items-center mt-5">
                 <hr className="flex-1 border-secondary-medium" />
                 <span className="text-secondary-solid">OR</span>
                 <hr className="flex-1  border-secondary-medium" />
               </div>
-              <Link
-                to="/login"
-                className="text-primary-solid text-base font-semibold block mt-4"
-              >
-                Login using phone number
-              </Link>
-              <Link
-                to="/login"
-                className="text-primary-solid text-base font-semibold block mt-4"
-              >
-                Login with a insurance account
-              </Link>
+              <LoginNavigationButtons
+                loginType={loginType}
+                setLoginType={setLoginType}
+              />
             </footer>
           </article>
         </div>
       </section>
+    </>
+  );
+}
+
+function Fields({ loginType, otpSent }) {
+  return (
+    <>
+      {loginType === "corporate" && (
+        <>
+          <TextField
+            id="corporateEmail"
+            placeholder="Enter your corporate email ID"
+            label="Enter Corporate Email ID"
+            className="text-left"
+            autoFocus
+            required
+            type="email"
+            inputClassName="placeholder:text-[#BDBDBD]"
+          />
+          <TextField
+            id="password"
+            placeholder="Enter Password"
+            label="Enter Password"
+            className="text-left mt-5"
+            required
+            type="password"
+            inputClassName="placeholder:text-[#BDBDBD]"
+          />
+        </>
+      )}
+      {loginType === "phone" && (
+        <>
+          <TextField
+            id="phoneNumber"
+            placeholder="Enter phone number"
+            label="Enter Phone Number"
+            className="text-left"
+            autoFocus
+            required
+            inputClassName="placeholder:text-[#BDBDBD]"
+          />
+          {otpSent && (
+            <TextField
+              id="otp"
+              placeholder="Enter OTP"
+              label="Enter OTP"
+              className="text-left mt-5"
+              required
+              type="password"
+              inputClassName="placeholder:text-[#BDBDBD]"
+            />
+          )}
+        </>
+      )}
+      {loginType === "insurance" && (
+        <>
+          <SelectField
+            defaultOptionLabel="Select your policy"
+            className="text-left"
+            selectClassName="w-full"
+            label="Your Policy"
+            id="policy"
+          >
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </SelectField>
+          <TextField
+            id="policyNumber"
+            placeholder="Enter your policy number"
+            label="Enter Your Policy Number"
+            className="text-left mt-5"
+            required
+            inputClassName="placeholder:text-[#BDBDBD]"
+          />
+        </>
+      )}
+    </>
+  );
+}
+
+function TermsAndPrivacy() {
+  return (
+    <p className="text-sm leading-6">
+      By signing in you agree to our <br />{" "}
+      <Link to="/terms-and-conditions" className="text-secondary-solid">
+        Terms and Conditions
+      </Link>{" "}
+      and{" "}
+      <Link to="/privacy-policy" className="text-secondary-solid">
+        Privacy Policy
+      </Link>
+    </p>
+  );
+}
+
+function LoginNavigationButtons({ loginType, setLoginType }) {
+  return (
+    <>
+      {(loginType === "phone" || loginType === "insurance") && (
+        <button
+          onClick={() => setLoginType("corporate")}
+          className="text-primary-solid text-base font-semibold block mt-4 mx-auto"
+        >
+          Login with a corporate account
+        </button>
+      )}
+      {(loginType === "insurance" || loginType === "corporate") && (
+        <button
+          onClick={() => setLoginType("phone")}
+          className="text-primary-solid text-base font-semibold block mt-4 mx-auto"
+        >
+          Login using phone number
+        </button>
+      )}
+      {(loginType === "phone" || loginType === "corporate") && (
+        <button
+          onClick={() => setLoginType("insurance")}
+          className="text-primary-solid text-base font-semibold block mt-4 mx-auto"
+        >
+          Login with a insurance account
+        </button>
+      )}
     </>
   );
 }
