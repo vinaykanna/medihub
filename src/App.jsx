992b52265ebdc78c0ef9de57dd99420ee73e5412
vinaydazn <vinaykanna367@gmail.com>
2024-12-20 23:18:27 +0530

    change base name react router

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <OnlineConsultationProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="medihub">
           <Routes />
         </BrowserRouter>
       </OnlineConsultationProvider>
